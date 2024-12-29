07a67e01f13aa8b67910db3791047716d79a508b
Renan Gemi Guerra <renangemi@icloud.com>
2024-12-29 00:45:30 -0300

    projeto inicial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank ");
+
+int [] idades = new int[5];
+idades[0] = 30;
+idades[0] = 40;
+idades[0] = 18;
+idades[0] = 55;
+idades[0] = 68;
+
+void TestaArray
